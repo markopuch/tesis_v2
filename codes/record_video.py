e29dad4f2ec4b716882d931df4b0d4f8f6cc9f0e
markopuch <40008292+markopuch@users.noreply.github.com>
2024-06-06 02:25:51 -0500

    update

--- codes/record_video.py
+++ codes/record_video.py
@@ -20,6 +20,8 @@ out = cv2.VideoWriter(f'video_{timestamp}.avi', fourcc, 20.0, (frame_width, fram
 
 print("Presiona 'a' para terminar la grabación.")
 
+img_counter = 0
+
 try:
     while True:
         ret, frame = cam.read()
@@ -35,16 +37,24 @@ try:
         #cv2.imshow('Original', frame) 
         
         # Wait for 'a' key to stop the program  
-        #if cv2.waitKey(1) & 0xFF == ord('a'): 
-        #    break
+        if cv2.waitKey(1) & 0xFF == ord('a'): 
+           break
         
         # Leer la entrada del teclado
         k = getch.getch()
-        print(k)
 
         if k == 'a':  # a
             print(" a presionado, terminando la grabación")
             break
+
+        elif k == ' ':
+            timestamp = time.strftime("%Y%m%d_%H%M%S") 
+            img_name = "foto_{}_{}.png".format(img_counter, timestamp)
+            cv2.imwrite(img_name, frame)
+            print("Foto tomada")
+            img_counter += 1
+            # Espera un momento para evitar múltiples capturas con una sola pulsación de espacio
+            time.sleep(0.5)
     
 finally:
     # Liberar la cámara y el escritor de video
